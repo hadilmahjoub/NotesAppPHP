0df9a4f45c4ec8b01431d5d73ee5bbdd83241a1b
hadil mahjoub <hadil.mahjoub@gmail.com>
2022-04-16 01:35:06 +0200

    updated style

--- note.php
+++ note.php
@@ -24,14 +24,24 @@ if (isset($_POST["trash"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/c096537dfc.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <title>Notes</title>
 </head>
 
 <body>
-    <nav class="navbar">
-        <a href="home.php">HOME</a>
-        <a href="note.php">NOTES</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a href="home.php" class="nav-link active">HOME</a>
+                </li>
+                <li class="nav-item">
+                    <a href="note.php" class="nav-link active">NOTES</a>
+                </li>
+            </ul>
+        </div>
     </nav>
 
     <div class="container">
@@ -49,14 +59,21 @@ if (isset($_POST["trash"])) {
         if (isset($_SESSION['todos'])) {    // if
             foreach ($_SESSION['todos'] as $key => $value) {    // foreach START
         ?>
-                <form method="POST" class="notepost">
+                <form method="POST" class="notepost" style="display: inline-block">
                     <!--Form for the delete-->
-                    <div class="titlepost"> <b><?= $value['title'] ?></b> </div>
-                    <div class="textpost"><?= $value['text'] ?></div>
-                    <input type="hidden" name="key" value="<?= $key ?>">
-                    <div>
-                        <button class="trash" name="trash"><i class="fa-solid fa-trash-can"></i></button>
+                    <div class="card" style="width: 18rem; display: inline-block">
+                        <div class="card-body">
+                            <div class="card-title"> <b><?= $value['title'] ?></b> </div>
+                            <hr>
+                            <div class="card-text"><?= $value['text'] ?></div>
+                            <input type="hidden" name="key" value="<?= $key ?>">
+                            <div>
+                                <button class="trash" name="trash" id="tra"><i class="fa-solid fa-trash-can"></i></button>
+                            </div>
+                        </div>
                     </div>
+
+
                 </form>
                 <!--Form for the delete END-->
 
